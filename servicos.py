c6f8c09ab8ede048539ac2113cdfd7a94788ceef
Thiago <thiago@fiap.com.br>
2026-09-03 20:05:52 +0000

    adicionado as portas alvo

---
+++ servicos.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+autor = "Thiago 189"
+#Lista: é uma sequencia ordenada de valores
+
+portas_alvo = [22, 80, 443, 3306, 8080, 3000, 5000, 8000, 137, 138, 139, 445]
+portas_alvo.append(21) #Adiciona a porta 21 na variavel portas_alvo
+
+print("A lista de portas é:",portas_alvo)
+print(f'A lista de portas é: {portas_alvo}')
