bce2ec73c78ab139d9b34b85c478aba78bb5916f
profthiago189 <150798447+profthiago189@users.noreply.github.com>
2026-09-04 16:47:42 -0300

    calcula media

--- calculo.py
+++ calculo.py
@@ -4,8 +4,11 @@ notas=[]
 soma=0
 
 for nota in range(1,6):
-  nota2 = float(input("Digite a nota %nota de 5: " %nota))
+  nota2 = float(input("Digite a nota %d de 5: " %nota))
   notas.append(nota2)
   soma += nota2
 
-for nota in range
+for nota in range[5]:
+  print("Nota %d: %.2f " %(nota+1, notas[nota]))
+
+print("A Media é %.2f " %(soma/5))
