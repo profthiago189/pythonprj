79fd41305fdf014e176101b2b586bfa048cadf46
profthiago189 <150798447+profthiago189@users.noreply.github.com>
2026-09-04 17:07:27 -0300

    finalizando o calculo da media

--- calculo.py
+++ calculo.py
@@ -1,14 +1,23 @@
-#!/usr/bin/env python3
+#!/usr/bin/env  python3
 
-notas=[]
-soma=0
+notas = []
+soma = 0
 
 for nota in range(1,6):
-  nota2 = float(input("Digite a nota %d de 5: " %nota))
+  while True:
+    try:
+        nota2 = float(input("Digite a nota %d de 5: " %nota))
+
+        if 0 <= nota2 <= 10:
+           break
+        print("Digite um nota entre 0 e 10.")
+    except ValueError:
+        print("Digite apenas numeros.")
+
   notas.append(nota2)
   soma += nota2
 
-for nota in range[5]:
-  print("Nota %d: %.2f " %(nota+1, notas[nota]))
+for nota in range(5):
+        print("Nota %d: %.2f " %(nota+1, notas[nota]))
 
-print("A Media é %.2f " %(soma/5))
+print ("A Media é %.2f " %(soma/5))
