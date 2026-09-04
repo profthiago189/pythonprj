9ec90c045afb57d6d7a5992453fbab086f1bc57e
profthiago189 <150798447+profthiago189@users.noreply.github.com>
2026-09-04 16:39:27 -0300

    criação do calculo

---
+++ calculo.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+notas=[]
+soma=0
+
+for nota in range(1,6):
+  nota2 = float(input("Digite a nota %nota de 5: " %nota))
+  notas.append(nota2)
+  soma += nota2
+
+for nota in range
